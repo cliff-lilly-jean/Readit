a0b6493d3823e60497d6bdf79ad8b9685bc635de
Cliff Jean <cliffjean@cliffs-mbp.attlocal.net>
2021-04-26 06:38:12 -0400

    Continued the design of the side navigation on the dashboard component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,40 +4,58 @@
       <div class="navigation">
         <ul>
           <li>
-            <span class="icon"><i class="fab fa-resolving"></i></span>
-            <span class="title"><h2>ReadIt</h2></span>
+            <a href="#">
+              <span class="icon"><i class="fab fa-resolving"></i></span>
+              <span class="title"><h2>ReadIt</h2></span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-home"></i></span>
-            <span class="title">Dashboard</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-home"></i></span>
+              <span class="title">Dashboard</span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-link"></i></span>
-            <span class="title">Connections</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-link"></i></span>
+              <span class="title">Connections</span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-book"></i></span>
-            <span class="title">Library</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-book"></i></span>
+              <span class="title">Library</span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-users"></i></span>
-            <span class="title">Clubs</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-users"></i></span>
+              <span class="title">Clubs</span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-question-circle"></i></span>
-            <span class="title">Help</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-question-circle"></i></span>
+              <span class="title">Help</span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-cog"></i></span>
-            <span class="title">Settings</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-cog"></i></span>
+              <span class="title">Settings</span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-lock"></i></span>
-            <span class="title">Password</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-lock"></i></span>
+              <span class="title">Password</span>
+            </a>
           </li>
           <li>
-            <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
-            <span class="title">Sign Out</span>
+            <a href="#">
+              <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
+              <span class="title">Sign Out</span>
+            </a>
           </li>
         </ul>
       </div>
@@ -92,5 +110,37 @@ export default {
 
 .navigation ul li:nth-child(1):hover {
   background: transparent;
+}
+
+.navigation ul li a {
+  position: relative;
+  display: block;
+  width: 100%;
+  display: flex;
+  text-decoration: none;
+  color: #fefefe;
+}
+
+.navigation ul li a .icon {
+  position: relative;
+  display: block;
+  min-width: 60px;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+}
+
+.navigation ul li a .icon .fa {
+  color: #fefefe;
+  font-size: 24px;
+}
+
+.navigation ul li a .title {
+  position: relative;
+  display: block;
+  padding: 0 10px;
+  height: 60px;
+  line-height: 60px;
+  white-space: nowrap;
 }
 </style>
